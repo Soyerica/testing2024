d8141fc0625553e2c2c6a87781434c5135ccb292
Soyerica <163366504+Soyerica@users.noreply.github.com>
2024-04-24 19:12:44 -0300

    Add files via upload
    actualizado 24-04

---
+++ script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    var email = document.getElementById("email").value;
+    var confirmEmail = document.getElementById("confirmEmail").value;
+
+    if (email !== confirmEmail) {
+        alert("Los correos electrónicos no coinciden.");
+        return false;
+    }
+
+    return true;
+}
